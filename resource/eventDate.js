dde4836b5faa7d98dc16d071e215a44d51b6cdf1
berkleylim <berkleylim16@gmail.com>
2023-06-20 23:03:54 +0900

    style: Update comments
    events 주석 추가

--- resource/eventDate.js
+++ resource/eventDate.js
@@ -1,3 +1,12 @@
+/**
+ * parameter
+ * start
+ * end
+ * startStr
+ * endStr
+ * timeZone
+ * 참조 : https://fullcalendar.io/docs/events-function
+ */
 // events = [
 //   {year: 2023, month:6, id:'d' startOfDay}
 // ]
